bdc2bb00152be87322e1b21f4cdf541d32d80ab7
matovu-farid <matovu90@gmail.com>
2022-02-21 17:11:20 +0300

    Add redux store

---
+++ src/redux/books/books.js
@@ -0,0 +1,7 @@
+const booksReducer = (state = { books: [] }, action) => {
+  switch (action.type) {
+    default: return state;
+  }
+};
+
+export default booksReducer;
---
+++ src/redux/configureStore.js
@@ -0,0 +1,7 @@
+import { createStore, combineReducers } from 'redux';
+import booksReducer from './books/books';
+import categoriesReducer from './categories/categories';
+
+const rootReducer = combineReducers({ books: booksReducer, categories: categoriesReducer });
+const store = createStore(rootReducer);
+export default store;
