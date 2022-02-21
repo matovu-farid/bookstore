36f9a6e8dd297cff7b40445948b20e0e41f052e8
matovu-farid <matovu90@gmail.com>
2022-02-21 17:26:37 +0300

    Add books actions

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,5 +1,21 @@
+const ADDBOOK = 'books/add';
+const REMOVEBOOK = 'books/remove';
+
+export const addBook = (book) => ({
+  type: ADDBOOK,
+  payload: book,
+});
+export const removeBook = (id) => ({
+  type: ADDBOOK,
+  payload: id,
+});
 const booksReducer = (state = { books: [] }, action) => {
   switch (action.type) {
+    case ADDBOOK: return { ...state, books: [...state.books, action.payload] };
+    case REMOVEBOOK: return {
+      ...state,
+      books: state.books.filter((book) => book.id !== action.payload),
+    };
     default: return state;
   }
 };
