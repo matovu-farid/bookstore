bc7a9958318f4a29107f1629f4ec32e347703b19
matovu-farid <matovu90@gmail.com>
2022-02-23 22:54:07 +0300

    Add basic item nodes

--- src/components/BookItem/BookItem.js
+++ src/components/BookItem/BookItem.js
@@ -1,6 +1,9 @@
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
+import { CircularProgressbar } from 'react-circular-progressbar';
 import { removeBook } from '../../redux/books/books';
+import 'react-circular-progressbar/dist/styles.css';
+import './BookItem.css';
 
 const BookItem = ({
   title, author, id,
@@ -10,11 +13,22 @@ const BookItem = ({
     dispatch(removeBook(id));
   };
   return (
-    <div>
-      <p>{title}</p>
+    <div className="item-card">
 
-      <p>{author}</p>
-      <button type="button" onClick={onClick}>Remove</button>
+      <div className="item-start">
+        <p>{title}</p>
+
+        <p>{author}</p>
+        <button type="button" onClick={onClick}>Remove</button>
+      </div>
+      <div className="item-middle">
+        <CircularProgressbar value={66} text={`${66}%`} />
+      </div>
+      <div>
+        <p>CURRENT CHAPTER</p>
+        <p>CHAPTER 17</p>
+        <button type="button">UPDATE PROGRESS</button>
+      </div>
     </div>
   );
 };
