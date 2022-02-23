9cfda3914294f71c532bcd8423c6a012b0626519
matovu-farid <matovu90@gmail.com>
2022-02-23 11:39:50 +0300

    Refactor add and remove to use thunks

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,14 +1,34 @@
 const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
 
-export const addBook = (payload) => ({
-  type: ADD_BOOK,
-  payload,
-});
-export const removeBook = (payload) => ({
-  type: REMOVE_BOOK,
-  payload,
-});
+const baseUrl = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/AcpJIcAvwt8ZMO0VHf3D/books';
+export const addBook = (payload) => async (dispatch) => {
+  await fetch(baseUrl, {
+    method: 'POST',
+    body: JSON.stringify(payload),
+    headers: {
+      'content-type': 'application/json',
+    },
+  });
+  dispatch({
+    type: ADD_BOOK,
+    payload,
+  });
+};
+
+export const removeBook = (payload) => async (dispatch) => {
+  const url = `${baseUrl}/${payload.id}`;
+  await fetch(url, {
+    method: 'DELETE',
+    headers: {
+      'content-type': 'application/json',
+    },
+  });
+  dispatch({
+    type: REMOVE_BOOK,
+    payload,
+  });
+};
 const booksReducer = (state = [], action) => {
   switch (action.type) {
     case ADD_BOOK: return [...state, action.payload];
