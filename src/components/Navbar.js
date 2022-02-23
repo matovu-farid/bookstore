9c85221b12b3cfff1ed20d293393df50488ae3d7
matovu-farid <matovu90@gmail.com>
2022-02-23 22:41:01 +0300

    style navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,18 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = () => (
-  <ul className="navbar">
-    <li>
-      <Link to="/">Books</Link>
-    </li>
-    <li>
-      <Link to="/categories">Categories</Link>
-    </li>
-  </ul>
+  <div className="nav-card">
+    <h3>Bookstore CMS</h3>
+    <ul className="navbar">
+      <li>
+        <NavLink className={({ isActive }) => (isActive ? 'active-nav' : 'non-active-nav')} to="/">Books</NavLink>
+      </li>
+      <li>
+        <NavLink className={({ isActive }) => (isActive ? 'active-nav' : 'non-active-nav')} to="/categories">Categories</NavLink>
+      </li>
+    </ul>
+  </div>
 );
 
 export default Navbar;
