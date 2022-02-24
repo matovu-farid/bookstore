4aa2ffac2215e95719bdf3c0df7d9a3f26cedcfc
matovu-farid <matovu90@gmail.com>
2022-02-24 14:21:55 +0300

    Center the nav and bookslist

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,15 +3,18 @@ import './Navbar.css';
 
 const Navbar = () => (
   <div className="nav-card">
-    <h3>Bookstore CMS</h3>
-    <ul className="navbar">
-      <li>
-        <NavLink className={({ isActive }) => (isActive ? 'active-nav' : 'non-active-nav')} to="/">Books</NavLink>
-      </li>
-      <li>
-        <NavLink className={({ isActive }) => (isActive ? 'active-nav' : 'non-active-nav')} to="/categories">Categories</NavLink>
-      </li>
-    </ul>
+
+    <div className="nav-content">
+      <h3>Bookstore CMS</h3>
+      <ul className="navbar">
+        <li>
+          <NavLink className={({ isActive }) => (isActive ? 'active-nav' : 'non-active-nav')} to="/">Books</NavLink>
+        </li>
+        <li>
+          <NavLink className={({ isActive }) => (isActive ? 'active-nav' : 'non-active-nav')} to="/categories">Categories</NavLink>
+        </li>
+      </ul>
+    </div>
   </div>
 );
 
